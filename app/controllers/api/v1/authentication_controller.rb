702b126d8efb912b21e8ca901a9c308103bcdf9a
raziel0110 <albangabriel42@gmail.com>
2024-04-01 19:08:40 +0300

    JWT Authentication

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      before_action :authorize_request, except: [:login]
+      rescue_from ActiveRecord::RecordNotFound, with: :handle_user_not_found
+
+      def login
+        @user = User.find_by_email(params[:email])
+
+        if @user&.authenticate(params[:password])
+          token = JsonWebToken.encode(user_id: @user.id)
+          time = Time.now + 1.hour.to_i
+
+          render json: {token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.username}, status: :ok
+        else
+          render json: {error: 'Unauthorized'}, status: :unauthorized
+        end
+      end
+
+      private
+
+      def authenticate_params
+        params.permit(:email, :password)
+      end
+
+      def handle_user_not_found(e)
+        render json: {error: 'User not found'}, status: :unauthorized
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_30_103933) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_200324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_30_103933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
