8de50c206d9e1a9493e08cd8ef05c1881c7d06cb
raziel0110 <albangabriel42@gmail.com>
2024-04-25 23:30:13 +0300

    Cors changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins 'http://localhost:3000', 'https://trackers-app-76d9dc18a2c1.herokuapp.com/'
 
     resource "*",
       headers: :any,
