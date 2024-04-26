c90cf30bb25fffab30d3b6b550a935d7600ab352
raziel0110 <albangabriel42@gmail.com>
2024-04-27 01:38:09 +0300

    fix

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -10,7 +10,7 @@ module Api
         if @user&.authenticate(params[:password])
           time = Time.now + 3600
           Rails.logger.info("\n\n\n\n\n\n #{time.utc.strftime('%m/%d/%Y %H:%M %p')} \n\n\n\n\n\n")
-          token = JsonWebToken.encode(user_id: @user.id, exp: time.utc.strftime('%m/%d/%Y %H:%M %p'), username: @user.username)
+          token = JsonWebToken.encode(user_id: @user.id, exp: time.strftime('%m/%d/%Y %H:%M %p'), username: @user.username)
 
           render json: {token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.username}, status: :ok
         else
