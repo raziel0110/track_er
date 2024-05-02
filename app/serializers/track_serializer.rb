e6b0ba806a1d4e9a8f2dcf5b622f5b85075cbe1c
raziel0110 <albangabriel42@gmail.com>
2024-05-02 10:25:20 +0300

    Modify url

--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,9 +1,14 @@
 class TrackSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :title, :url
+  attributes :id, :title, :url, :content_type
 
   def url
-    url_for(object.filename)
+    # url_for(object.filename)
+    Rails.application.routes.url_helpers.blob_url(object.filename)
+  end
+
+  def content_type
+    object.filename.content_type
   end
 end
