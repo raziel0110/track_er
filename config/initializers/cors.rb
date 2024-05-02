717f4cfd816f0f90ab8c6781771c972f69ecab07
raziel0110 <albangabriel42@gmail.com>
2024-05-02 14:35:26 +0300

    test cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,20 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://trackers-app-76d9dc18a2c1.herokuapp.com/'
+    # origins 'http://localhost:3000', 'https://trackers-app-76d9dc18a2c1.herokuapp.com/'
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    allow do
+      origins 'https://trackers-app-76d9dc18a2c1.herokuapp.com/'
+      resource '/api/*', headers: :any, methods: [:get, :post, :options, :put, :delete]
+    end
+
+    allow do
+      origins 'http://localhost:3000'
+      resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
+    end
+
+    # resource "*",
+    #   headers: :any,
+    #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
